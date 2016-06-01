700f72e213c5607a7a354dcaabe291d0aa3e2af7
Jake Cataford <jakecataford@gmail.com>
2016-06-01 16:40:32 -0400

    Initial Commit

---
+++ Erp.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Erp {
+	public class Erp {
+		public enum InterpolationFunctions {
+			EaseIn
+		}
+
+
+		public static void Interpolate(float a, float b, float pct) {
+
+		}
+
+		private static IInterpolator getInterpolator(InterpolationFunctions function) {
+			switch (function) {
+			case InterpolationFunctions.
+			}
+		}
+	}
+}
---
+++ Interpolators/Base.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Erp {
+	public interface IInterpolator {
+		void GetValue(float start, float end, float percentage);
+	}
+}
